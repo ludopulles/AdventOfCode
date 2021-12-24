e71229317cc4d26e742971f1c723439f130c47f6
Ludo Pulles <ludo.pulles@gmail.com>
2021-12-24 15:53:45 +0100

    Show delta time

--- delta-t-scoreboard/scoreboard.py
+++ delta-t-scoreboard/scoreboard.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import datetime, json, sys
 
 if len(sys.argv) != 2 and len(sys.argv) != 3:
@@ -25,10 +26,17 @@ def delta_t(_id, day):
         return None
     return (today['2']['get_star_ts'] - today['1']['get_star_ts'], _id)
 
+def hms(t):
+    if t is None:
+        return ""
+    return (("" if t[0] < 3600 else str(t[0]//3600) + "h")
+        + ("" if t[0] < 60 else str((t[0]//60)%60) + "m")
+        + str(t[0]%60) + "s")
+
 def scoring(rank): # rank is zero based.
     N = len(members)
-    # return pow(N, 1.0 - rank / (N - 1))
-    return len(members) - rank
+    return pow(N, 1.0 - rank / (N - 1))
+    # return len(members) - rank
 
 
 for day in range(1, curday + 1):
@@ -44,10 +52,17 @@ for day in range(1, curday + 1):
 
 deltas = dict([ (times[i][1], scoring(ranks[i])) for i in range(len(ranks)) ])
 fscores = sorted(scores.items(), key=lambda x: -x[1])
-print("Day ", curday, "\nRank | Score  | Delta  | Name", sep="")
+deltatimes = [ hms(delta_t(x[0], curday)) for x in fscores ]
+W = max(4, max(map(len, deltatimes)))
+
+print("Day ", curday, "\nRank |  Score |  Delta | ",
+    ("{:>" + str(W) + "}").format("time"),
+    " | Name", sep="")
 for i in range(len(fscores)):
     _id = fscores[i][0]
     d = f"{deltas[_id]:+.2f}" if _id in deltas else ""
     s = round(fscores[i][1], 2)
-    print(f"{i+1:>4} | {fscores[i][1]:>6.2f} | {d:>6} | ", names[fscores[i][0]], sep="")
+    print(f"{i+1:>4} | {fscores[i][1]:>6.2f} | {d:>6} | ",
+            ("{:>" + str(W) + "}").format(deltatimes[i]),
+            " | ", names[fscores[i][0]], sep="")
 print()
