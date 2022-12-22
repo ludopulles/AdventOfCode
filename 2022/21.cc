47fb6a057fe36047d55750eb67098a3e2f01e051
Ludo Pulles <ludo.pulles@gmail.com>
2022-12-22 12:44:55 +0100

    Faster algo for day 21 part b

--- 2022/21.cc
+++ 2022/21.cc
@@ -13,7 +13,7 @@ typedef vector<ii> vii;
 #define x first
 #define y second
 
-typedef double T;
+typedef ll T;
 
 struct Node {
 	char op;
@@ -37,26 +37,81 @@ T operate(char op, T L, T R) {
 	if (op == '+') return L + R;
 	if (op == '-') return L - R;
 	if (op == '*') return L * R;
-	if (op == '/') return L / R;
+	if (op == '/') { assert(L % R == 0); return L / R; }
 	assert(false);
 }
 
 T dfs(string name) {
-	assert(nodes.find(name) != nodes.end());
+	// assert(nodes.find(name) != nodes.end());
 	Node &node = nodes[name];
 
 	if (node.op == ' ')
 		return node.val;
 
 	node.val = operate(node.op, dfs(node.lop), dfs(node.rop));
-	// printf("Monkey %s: %f\n", name.c_str(), node.val);
-	// return node.val = operate(node.op, dfs(node.lop), dfs(node.rop));
 	return node.val;
 }
 
 T evalF(T inp) {
 	nodes["humn"].val = inp;
 	return dfs(nodes["root"].lop) - dfs(nodes["root"].rop);
+}
+
+bool has_humn(string name) {
+	Node &node = nodes[name];
+	return name == "humn" || (node.op != ' ' && (has_humn(node.lop) || has_humn(node.rop)));
+}
+
+T solve(string name, T x) {
+	if (name == "humn") return x;
+	Node &node = nodes[name];
+	assert(node.op != ' ' && has_humn(name));
+
+	if (has_humn(node.lop)) {
+		T y, z = nodes[node.rop].val;
+		printf("Solve: %lld = ? %c %lld\n", x, node.op, z);
+
+		switch (node.op) {
+		case '+': y = x - z; break;
+		case '-': y = x + z; break;
+		case '*': assert(x % z == 0); y = x / z; break;
+		case '/': y = x * z; break;
+		default: assert(false);
+		}
+		return solve(node.lop, y);
+	} else {
+		T y, z = nodes[node.lop].val;
+		printf("Solve: %lld = %lld %c ?\n", x, z, node.op);
+
+		switch (node.op) {
+		case '+': y = x - z; break;
+		case '-': y = z - x; break;
+		case '*': assert(x % z == 0); y = x / z; break;
+		case '/': assert(z % x == 0); y = z / x; break;
+		default: assert(false);
+		}
+		return solve(node.rop, y);
+	}
+}
+
+T binary_search() {
+	T lo = -1e18, hi = 1e18;
+	// printf("%lld -> %f\n", (ll) lo, evalF(lo));
+	// printf("%lld -> %f\n", (ll) hi, evalF(hi));
+
+	if (evalF(lo) > evalF(hi)) swap(lo, hi);
+
+	REP(i, 100) {
+		T mid = (lo + hi) / 2.0;
+		T em = evalF(mid);
+		// printf("We have %f (%f) -> %f\n", mid, abs(hi - lo), em);
+		if (em < 0) lo = mid;
+		else hi = mid;
+	}
+
+	// printf("%lld -> %f\n", (ll) lo, evalF(lo));
+	// printf("%lld -> %f\n", (ll) hi, evalF(hi));
+	return (lo + hi) / 2.0;
 }
 
 int main() {
@@ -69,31 +124,20 @@ int main() {
 			int pos = 6;
 			ll val = parseInt(line, pos);
 
-			assert(nodes.find(name) == nodes.end());
+			// assert(nodes.find(name) == nodes.end());
 			nodes[ name ] = Node{' ',"","", (T) val};
 		}
 	}
 
 	T solA = dfs("root");
-	printf("Part A: %.10f\n", solA);
 
+	Node rt = nodes["root"];
+	string part = has_humn(rt.lop) ? rt.lop : rt.rop;
+	T solve_value = nodes[has_humn(rt.lop) ? rt.rop : rt.lop].val;
+	T solB = solve(part, solve_value);
 
-	T lo = -1e20, hi = 1e20;
-	printf("%lld -> %f\n", (ll) lo, evalF(lo));
-	printf("%lld -> %f\n", (ll) hi, evalF(hi));
-
-	if (evalF(lo) > evalF(hi)) swap(lo, hi);
-
-	while (abs(hi - lo) > 1e-10) {
-		T mid = (lo + hi) / 2.0;
-		T em = evalF(mid);
-		printf("We have %f -> %f\n", mid, em);
-		if (em < 0) lo = mid;
-		else hi = mid;
-	}
-
-	printf("%lld -> %f\n", (ll) lo, evalF(lo));
-	printf("%lld -> %f\n", (ll) hi, evalF(hi));
-
+	printf("\n");
+	printf("Part A: %lld\n", (ll) solA);
+	printf("Part B: %lld\n", (ll) solB);
 	return 0;
 }
