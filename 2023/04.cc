f10791bdb37fc3366eb1eb53c7d36f09f5b4ebe2
Ludo Pulles <ludo.pulles@gmail.com>
2023-12-04 11:20:31 +0100

    Day 4

--- 2023/04.cc
+++ 2023/04.cc
@@ -44,12 +44,38 @@ pair<string, string> split_at(string s, string split) {
 }
 
 
+
 int main() {
 	ll sumA = 0, sumB = 0;
 
 	string line;
+	vector<int> ncards;
 	while (getline(cin, line), cin) {
-		
+		auto [game, scard] = split_at(line, ": ");
+		auto [winning, having] = split_at(scard, " | ");
+
+		vector<string> _wins = split_str(winning, " ");
+		set<string> wins = set<string>(all(_wins));
+		wins.erase("");
+
+		int nfound = 0;
+		for (string have : split_str(having, " ")) {
+			if (have == "") continue;
+			nfound += wins.find(have) != wins.end();
+		}
+		ncards.pb(nfound);
+
+		if (nfound) sumA += 1 << (nfound - 1);
+	}
+
+	vector<int> nwins(sz(ncards), 0);
+	for (int i = sz(ncards) - 1; i >= 0; i--) {
+		nwins[i] = 1;
+		if (ncards[i]) {
+			for (int j = 1; j <= ncards[i]; j++)
+				nwins[i] += nwins[i + j];
+		}
+		sumB += nwins[i];
 	}
 
 	cout << "Part A: " << sumA << endl;
