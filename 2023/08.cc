88c4e9a6382e4083c3e6e39beb25c76159928a92
Ludo Pulles <ludo.pulles@gmail.com>
2023-12-08 13:51:44 +0100

    Day 8

---
+++ 2023/08.cc
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+#define rep(i,a,b) for (auto i = (a); i < (b); ++i)
+#define REP(i,n) rep(i,0,n)
+#define sz(v) ((int) (v).size())
+#define all(v) begin(v), end(v)
+#define pb push_back
+#define eb emplace_back
+#define x first
+#define y second
+
+string route;
+unordered_map<string, string> L, R;
+vector<unordered_map<string, pair<string, ll>>> to_next_Z;
+
+ll gcd(ll a, ll b) {
+	while (b) { a %= b; swap(a, b); }
+	return a;
+}
+
+pair<string, ll> num_steps(const int i, const string &s) {
+	if (to_next_Z[i].find(s) == to_next_Z[i].end()) {
+		int idxRoute = i, steps = 0;
+		string cur = s;
+		// set<pair<string, ll>> visited;
+		do {
+			// if (visited.find(make_pair(cur, idxRoute)) != visited.end()) break;
+			// visited.emplace(cur, idxRoute);
+			cur = (route[idxRoute] == 'L' ? L : R)[cur];
+			steps++;
+			if (++idxRoute == sz(route)) idxRoute = 0;
+		} while (cur[2] != 'Z');
+
+		assert(cur[2] == 'Z');
+		to_next_Z[i][s] = make_pair(cur, steps);
+	}
+	return to_next_Z[i][s];
+}
+
+
+int main() {
+	ll sumA = 0, sumB = 0;
+	vector<string> nodes, curnodes;
+	string line;
+
+	getline(cin, route);
+	getline(cin, line);
+	while (getline(cin, line), cin) {
+		string fr = line.substr(0, 3), toL = line.substr(7, 3), toR = line.substr(12, 3);
+		nodes.pb(fr);
+		if (fr[2] == 'A') curnodes.pb(fr);
+		L[fr] = toL;
+		R[fr] = toR;
+	}
+
+	string cur = "AAA";
+	int idxRoute = 0;
+	if (find(all(nodes), cur) != nodes.end()) {
+		while (cur != "ZZZ") {
+			if (route[idxRoute] == 'L') cur = L[cur];
+			else cur = R[cur];
+			sumA++;
+
+			if (++idxRoute == sz(route)) idxRoute = 0;
+		}
+
+		cout << "Part A: " << sumA << endl;
+	}
+
+	to_next_Z.resize(sz(route));
+
+	int n = curnodes.size();
+
+	sumB = 1;
+	for (int i = 0; i < n; i++) {
+		int st = 0;
+		string cur = curnodes[i];
+		while (to_next_Z[st % sz(route)].find(cur) == to_next_Z[st % sz(route)].end()) {
+			auto [nxt, add] = num_steps(st % sz(route), cur);
+			cur = nxt;
+			st += add;
+		}
+
+		auto [nxt, add] = to_next_Z[st % sz(route)][cur];
+		assert(cur == nxt && add % sz(route) == 0);
+		assert(st % add == 0);
+
+		sumB = sumB / gcd(sumB, add) * add;
+	}
+
+	cout << "Part B: " << sumB << endl;
+	return 0;
+}
