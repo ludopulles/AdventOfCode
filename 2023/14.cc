ec69285ccba645d4d5af7dda9f9457780b0a7aa7
Ludo Pulles <ludo.pulles@gmail.com>
2023-12-14 12:10:19 +0100

    Day 14

---
+++ 2023/14.cc
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+#define rep(i,a,b) for (auto i = (a); i < (b); ++i)
+#define REP(i,n) rep(i,0,n)
+#define sz(v) ((int) (v).size())
+#define all(v) begin(v), end(v)
+#define pb push_back
+#define eb emplace_back
+#define x first
+#define y second
+
+/**************************************************************************************************
+ * Code for today is below:
+ *************************************************************************************************/
+
+ll eval(const vector<string> &grid) {
+	ll res = 0;
+	for (int row = 0; row < sz(grid); row++) {
+		for (int col = 0; col < sz(grid[row]); col++) {
+			if (grid[row][col] == 'O')
+				res += sz(grid) - row;
+		}
+	}
+	return res;
+}
+
+ll partA(vector<string> grid) {
+	for (int row = 0; row < sz(grid); row++) {
+		for (int col = 0; col < sz(grid[row]); col++) {
+			if (grid[row][col] != 'O') continue;
+
+			// move up
+			int r = row;
+			while (r > 0 && grid[r - 1][col] == '.' && grid[r][col] == 'O') {
+				swap(grid[r - 1][col], grid[r][col]);
+				r--;
+			}
+		}
+	}
+
+	return eval(grid);
+}
+
+void rotateRound(vector<string> &grid) {
+	for (int row = 0; row < sz(grid); row++) {
+		for (int col = 0; col < sz(grid[row]); col++) {
+			if (grid[row][col] != 'O') continue;
+			int r = row; // move up
+			while (r > 0 && grid[r - 1][col] == '.' && grid[r][col] == 'O') {
+				swap(grid[r - 1][col], grid[r][col]); r--;
+			}
+		}
+	}
+
+	for (int row = 0; row < sz(grid); row++) {
+		for (int col = 0; col < sz(grid[row]); col++) {
+			if (grid[row][col] != 'O') continue;
+			int c = col; // move left
+			while (c > 0 && grid[row][c - 1] == '.' && grid[row][c] == 'O') {
+				swap(grid[row][c - 1], grid[row][c]); c--;
+			}
+		}
+	}
+
+	for (int row = sz(grid) - 1; row >= 0; row--) {
+		for (int col = 0; col < sz(grid[row]); col++) {
+			if (grid[row][col] != 'O') continue;
+			int r = row; // move down
+			while (r + 1 < sz(grid) && grid[r + 1][col] == '.' && grid[r][col] == 'O') {
+				swap(grid[r + 1][col], grid[r][col]); r++;
+			}
+		}
+	}
+
+	for (int row = 0; row < sz(grid); row++) {
+		for (int col = sz(grid[row]) - 1; col >= 0; col--) {
+			if (grid[row][col] != 'O') continue;
+			int c = col; // move right
+			while (c + 1 < sz(grid[row]) && grid[row][c + 1] == '.' && grid[row][c] == 'O') {
+				swap(grid[row][c + 1], grid[row][c]); c++;
+			}
+		}
+	}
+}
+
+ll partB(vector<string> grid) {
+	vector<vector<string>> seq;
+	map<vector<string>, int> lookup;
+
+	while (lookup.find(grid) == lookup.end()) {
+		lookup[grid] = sz(seq);
+		seq.pb(grid);
+		rotateRound(grid);
+	}
+
+	ll precyc = lookup[grid], cycle_len = seq.size() - precyc;
+	ll num = precyc + (1'000'000'000LL - precyc) % cycle_len;
+	return eval(seq[num]);
+}
+
+
+
+int main() {
+	vector<string> grid;
+	string line;
+	while (getline(cin, line), cin) grid.pb(line);
+
+	ll sumA = partA(grid);
+	cout << "Part A: " << sumA << endl;
+	ll sumB = partB(grid);
+	cout << "Part B: " << sumB << endl;
+	return 0;
+}
