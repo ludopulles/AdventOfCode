d25c2d29fcb5428617eccaf8ecec574ec0b90001
Ludo Pulles <ludo.pulles@gmail.com>
2023-12-21 13:24:07 +0100

    Day 20, part B

--- 2023/20.cc
+++ 2023/20.cc
@@ -9,43 +9,58 @@ vector<string> toS[MAXN];
 vi to[MAXN];
 
 map<string, int> lookup;
-bool state[MAXN] = {};
+bool state[MAXN] = {}, partB;
 map<int, bool> wason[MAXN];
 
-int lowsent = 0, highsent = 0;
+int lowsent = 0, highsent = 0, idxGH;
 
-bool sendPulse() {
+void sendPulse() {
 	queue<tuple<int,int,int>> q;
 	q.emplace(broadcaster, broadcaster, false);
 
-	bool saw_rx = false;
-
 	while (!q.empty()) {
 		auto [fr, idx, isHigh] = q.front(); q.pop();
-		// cerr << nodes[fr] << " -" << (isHigh ? "high" : "low") << "-> " << nodes[idx] << endl;
 		if (isHigh) highsent++;
 		else lowsent++;
 
-		if (idx == nnodes) {
-			if (!isHigh && idx == lookup["rx"]) saw_rx = true;
-			continue;
-		}
+		if (idx == nnodes) continue;
 
-		if (idx == lookup["broadcaster"]) {
+		if (idx == broadcaster) {
 			for (int t : to[idx]) q.emplace(idx, t, isHigh);
 		} else if (type[idx] == '%') {
 			if (!isHigh) {
 				bool send = state[idx] = !state[idx];
 				for (int t : to[idx]) q.emplace(idx, t, send);
 			}
-		} else {
+		} else { assert(type[idx] == '&');
 			bool send = true;
 			wason[idx][fr] = isHigh;
 			for (auto [loc, val] : wason[idx]) send &= val;
 			for (int t : to[idx]) q.emplace(idx, t, !send);
 		}
 	}
-	return saw_rx;
+}
+
+bool sendPartPulse(int bnode, int enode) {
+	queue<tuple<int,int,int>> q;
+	q.emplace(broadcaster, bnode, false);
+	bool sent = false;
+	while (!q.empty()) {
+		auto [fr, idx, isHigh] = q.front(); q.pop();
+		if (idx == enode) { sent |= !isHigh; continue; }
+		if (type[idx] == '%') {
+			if (!isHigh) {
+				bool send = state[idx] = !state[idx];
+				for (int t : to[idx]) q.emplace(idx, t, send);
+			}
+		} else { assert(type[idx] == '&');
+			bool send = true;
+			wason[idx][fr] = isHigh;
+			for (auto [loc, val] : wason[idx]) send &= val;
+			for (int t : to[idx]) q.emplace(idx, t, !send);
+		}
+	}
+	return sent;
 }
 
 vi lows, highs;
@@ -63,49 +78,48 @@ int main() {
 		lookup[nodes[nnodes]] = nnodes;
 		nnodes++;
 	}
+
+	partB = lookup.find("gh") != lookup.end();
+	idxGH = partB ? lookup["gh"] : -1;
+	nodes[nnodes] = "???";
 	broadcaster = lookup["broadcaster"];
 	REP(i, nnodes) for (const string &s : toS[i]) {
 		if (lookup.find(s) == lookup.end()) to[i].pb(nnodes);
 		else to[i].pb(lookup[s]), wason[lookup[s]][i] = false;
 	}
 
-	ll fst_rx = LLONG_MAX;
-	// ll cyc_start = -1;
-	REP(_, 1000) {
-		if (sendPulse())
-			fst_rx = min(fst_rx, (ll) _ + 1);
-/*
-		REP(i, nnodes) {
-			if (type[i] == '%') cerr << state[i];
-			else if (type[i] == '&') for (auto [j, v] : wason[i]) cerr << v;
-		}
-		cerr << endl;
-
-		vector<bool> cur;
-		REP(i, nnodes) {
-			if (type[i] == '%') cur.pb(state[i]);
-			else if (type[i] == '&') for (auto [j, v] : wason[i]) cur.pb(v);
-		}
-
-		lows.pb(lowsent);
-		highs.pb(highsent);
-		if (state_map.find(cur) != state_map.end()) {
-			cyc_start = state_map.find(cur)->y;
-			break;
-		}
-		state_map[cur] = sz(lows) - 1; */
-	}
-
-	// cout << "Cycle length: " << sz(lows) << " but precycle is " << cyc_start << endl;
-	// cout << "Low: " << lowsent << ", high: " << highsent << endl;
-
+	// Part A:
+	REP(_, 1000) sendPulse();
 	sumA = lowsent * highsent;
 	cout << "Part A: " << sumA << endl;
 
-	assert(fst_rx == LLONG_MAX);
-	fst_rx = 1000;
-	while (!sendPulse()) fst_rx++;
-	sumB = fst_rx;
+	// Part B:
+	if (!partB) return 0;
+	REP(i, nnodes) state[i] = false;
+	REP(i, nnodes) for (const string &s : toS[i])
+		if (lookup.find(s) != lookup.end()) wason[lookup[s]][i] = false;
+
+	sumB = 1LL;
+	for (ll bnode : to[broadcaster]) {
+		ll enode = -1;
+		vi canReach = {bnode};
+		for (int i = 0; i < sz(canReach); i++) {
+			if (sz(to[canReach[i]]) == 1 && to[canReach[i]][0] == idxGH) {
+				enode = canReach[i];
+				continue;
+			}
+
+			for (auto j : to[canReach[i]])
+				if (find(all(canReach), j) == canReach.end()) canReach.pb(j);
+		}
+
+		ll its1 = 0, its2 = 0;
+		do { its1++; } while (!sendPartPulse(bnode, enode));
+		do { its2++; } while (!sendPartPulse(bnode, enode));
+		assert(its1 == its2 && gcd(sumB, its1) == 1);
+		sumB *= its1;
+	}
+
 	cout << "Part B: " << sumB << endl;
 	return 0;
 }
